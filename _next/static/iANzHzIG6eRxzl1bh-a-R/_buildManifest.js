a6143f5dcffd02881a03502a08c8b70da54d8a4b
this-is-cris <this-is-cris@users.noreply.github.com>
2022-10-12 10:30:19 +0000

    deploy: d294b754b255b91349be05221aea899ac3497f27

---
+++ _next/static/iANzHzIG6eRxzl1bh-a-R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/377-0bec6c4ac211603a.js","static/chunks/pages/index-0e87ae4dbe9bc610.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
