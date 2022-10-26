5350f5a03126bd8f064cedfbf57d33708ec5318a
zimrick <zimrick@users.noreply.github.com>
2022-10-26 09:54:32 +0000

    deploy: fde4346b68cf8474f1f8d7918e252ea1c9fb8b10

--- _next/static/z1r_mLCh3n-aSMrHinNgl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/556-3893b859b89bdfab.js","static/chunks/pages/index-cdfb70506ed4c8e9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
