b7be8d4ab626299e6044720638bd2dd14c4ee4d1
zimrick <richard@zcreativelabs.com>
2024-10-20 12:48:47 +0200

    Hooked up newsletter signup

--- src/utils/mailchimp.js
+++ src/utils/mailchimp.js
@@ -71,8 +71,8 @@ const addToMailchimp = function addToMailchimp(email, fields, endpointOverride) 
         });
     }
 
-    let endpoint = process.env.MAILCHIMP_ADDRESS; // eslint-disable-line no-undef
-    const timeout = process.env.MAILCHIMP_TIMEOUT; // eslint-disable-line no-undef
+    let endpoint = process.env.NEXT_PUBLIC_MAILCHIMP_ADDRESS; // eslint-disable-line no-undef
+    const timeout = process.env.NEXT_PUBLIC_MAILCHIMP_TIMEOUT; // eslint-disable-line no-undef
 
     // The following tests for whether you passed in a `fields` object. If
     // there are only two params and the second is a string, then we can safely
